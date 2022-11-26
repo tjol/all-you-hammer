25a4a9f49eed96d94f85fb0f837112d7b639f919
Thomas Jollans <tjol@tjol.eu>
2022-11-26 18:21:33 +0100

    Add an end to the level, plus additional levels

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ import dropHammerShape from './sprites/drop-hammer.shape.json'
 import startBtnImg from './sprites/png/start-btn.png'
 import restartBtnImg from './sprites/png/restart-btn.png'
 import tiltBtnImg from './sprites/png/tilt-btn.png'
+import nextLevelImg from './sprites/png/next-level.png'
 
 import nail1SoundMp3 from './audio/nail1.mp3'
 import nail1SoundOgg from './audio/nail1.ogg'
@@ -43,6 +44,30 @@ const hammerTargetPos = 400
 const hudPos = [10, 10]
 const redAlertPos = [500, 300]
 
+const seeds = [
+  'all you have is two hammers, one visible and one invisible',
+  'Thor, God of Hammers, would have been very confused indeed',
+  'level the third, the third level'
+]
+const level1Height = 4500
+const levelHeightIncrement = 1000
+
+let level = {
+  number: 1,
+  seed: seeds[0],
+  height: level1Height
+}
+
+let tiltCount = 0
+
+function seedForLevel (number) {
+  if (number <= seeds.length) {
+    return seeds[number - 1]
+  } else {
+    return `level #${number}`
+  }
+}
+
 class AllYouHaveIsAHammer extends Phaser.Scene {
   preload () {
     this.load.spritesheet('nails', nailSprites, {
@@ -57,6 +82,7 @@ class AllYouHaveIsAHammer extends Phaser.Scene {
     this.load.image('nail-head', nailHeadImg)
     this.load.image('restart-btn', restartBtnImg)
     this.load.image('tilt-btn', tiltBtnImg)
+    this.load.image('next-level', nextLevelImg)
 
     this.load.audio('nail1', [nail1SoundOgg, nail1SoundMp3])
     this.load.audio('nail2', [nail2SoundOgg, nail2SoundMp3])
@@ -64,9 +90,12 @@ class AllYouHaveIsAHammer extends Phaser.Scene {
     this.load.audio('screw', [screwSoundOgg, screwSoundMp3])
 
     this._yPos = 0
-    this._tiltCount = 0
+    // this._tiltCount = 0
     this._nails = {}
-    this._seed = 'all you have is two hammers, one visible and one invisible'
+    this._levelNumber = level.number
+    this._seed = level.seed
+    this._maxNailY = level.height
+    this._maxCameraY = level.height + 100
     this._width = this.sys.game.canvas.width
     this._height = this.sys.game.canvas.height
   }
@@ -89,9 +118,8 @@ class AllYouHaveIsAHammer extends Phaser.Scene {
 
     // create the outer walls
     this.matter.add.rectangle(-5, 100000, 10, 200000, { isStatic: true })
-    this.matter.add.rectangle(this._width + 5, 100000, 10, 200000, {
-      isStatic: true
-    })
+    this.matter.add.rectangle(this._width + 5, 100000, 10, 200000, { isStatic: true })
+    this.matter.add.rectangle(this._width / 2, this._maxCameraY + this._height, this._width, 10, { isStatic: true })
 
     // create the nails
     this._createNails()
@@ -143,31 +171,58 @@ class AllYouHaveIsAHammer extends Phaser.Scene {
       [this._restartBtn, this._restartBtn.y],
       [this._tiltBtn, this._tiltBtn.y]
     ]
+
+    this._nextLevelButton = null
   }
 
   update () {
     const yPos = this._dropHammer.y - hammerTargetPos
     if (yPos > this._yPos) {
-      this._yPos = yPos
-      this.cameras.main.scrollY = yPos
+      this._yPos = Math.min(yPos, this._maxCameraY)
+      this.cameras.main.scrollY = this._yPos
       this._collectGarbage()
       this._createNails()
+    }
+    if (this._yPos === this._maxCameraY && this._nextLevelButton === null) {
+      // Level over
+      const levelNumber = this._levelNumber
+      window.setTimeout(() => this._createNextLevelButton(levelNumber), 3000)
     }
 
     for (const [obj, fixedY] of this._fixedObjects) {
       obj.y = this._yPos + fixedY
     }
 
-    let hudText = `${Math.round(this._dropHammer.y / nailDist * 10)}`
-    if (this._tiltCount !== 0) {
-      hudText += ` - ${this._tiltCount} TILT`
+    let hudText = `LEVEL ${this._levelNumber} - ${Math.round(this._yPos / nailDist * 10)}`
+    if (tiltCount !== 0) {
+      hudText += ` - ${tiltCount} TILT`
     }
     this._hud.setText(hudText)
   }
 
+  _createNextLevelButton (levelNumber) {
+    if (this._nextLevelButton === null && levelNumber === this._levelNumber) {
+      this._nextLevelButton = this.add.image(750, 500 + this._yPos, 'next-level')
+      this._nextLevelButton.setScale(0.5)
+      this._nextLevelButton.setDepth(100)
+      this._nextLevelButton.setInteractive()
+      this._nextLevelButton.on('clicked', this._goToNextLevel, this)
+    }
+  }
+
+  _goToNextLevel () {
+    const levelNumber = level.number + 1
+    level = {
+      number: levelNumber,
+      seed: seedForLevel(levelNumber),
+      height: level1Height + (levelNumber - 1) * levelHeightIncrement
+    }
+    this._restart()
+  }
+
   _createNails () {
     let y0 = Math.max(this._yPos, 300)
-    const yEnd = this._height + this._yPos + 2 * nailDist
+    const yEnd = Math.min(this._height + this._yPos + 2 * nailDist, this._maxNailY)
     // place nails at odd multiples of nailDist/2
     y0 = Math.ceil(y0 / nailDist / 2) * nailDist * 2
     const x0 = nailDist / 2
@@ -316,7 +371,7 @@ class AllYouHaveIsAHammer extends Phaser.Scene {
   _tilt () {
     this._dropHammer.x += (Math.random() - 0.5) * nailDist
     this._dropHammer.y += (Math.random() - 0.5) * nailDist
-    this._tiltCount += 1
+    tiltCount += 1
   }
 }
 
